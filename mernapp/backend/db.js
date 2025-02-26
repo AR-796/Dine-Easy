f7430acc5a078a45fb13d8c6390dc8acef7fea35
AR-796 <mdabdulrahman0122@gmail.com>
2025-02-26 13:16:10 -0600

    Initial commit

---
+++ mernapp/backend/db.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+
+const mongoDB = async () => {
+  try {
+    await mongoose.connect('mongodb+srv://FoodFusion:FoodFusion@cluster0.b3idu.mongodb.net/FoodFusionmern?retryWrites=true&w=majority&appName=Cluster0', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('Connected to MongoDB');
+
+    // Access the database and collections
+    const db = mongoose.connection.db;
+
+    // Fetch foodData
+    const foodData = await db.collection("foodData").find({}).toArray();
+    global.foodData = foodData;
+
+    // Fetch foodCategory
+    const foodcategory = await db.collection("foodcategory").find({}).toArray();
+
+    // Assign the data to global variables
+    global.foodcategory = foodcategory;
+
+    // Log the data
+    console.log(global.foodData);
+    console.log(global.foodcategory);
+
+  } catch (error) {
+    console.error('Failed to connect to MongoDB:', error.message);
+  }
+};
+
+module.exports = mongoDB;
+
+
